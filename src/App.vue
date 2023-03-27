be63142dc464c82b3db6294aada4396e8863de35
giovannifranchi <giovanni.franchi95@gmail.com>
2023-03-27 18:39:28 +0200

    Feat: bonus spinner

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,28 @@ import { store } from './store';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import SpinnerLoradVue from './components/SpinnerLorad.vue';
   export default {
     name: 'App',
     components: {
       AppHeader,
       AppMain,
       AppFooter,
+      SpinnerLoradVue,
     },
     data(){
       return {
         store,
+        isBusy: true,
       }
     },
     created(){
       axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
       .then(response => {
         store.cards = response.data;
+      })
+      .then(()=>{
+        this.isBusy = false;
       });
     }
   }
@@ -27,8 +33,8 @@ import AppFooter from './components/AppFooter.vue';
 
 <template>
   <AppHeader/>
-  <AppMain/>
-  <AppFooter/>
+  <SpinnerLoradVue v-if="isBusy"/>
+  <AppMain v-else/>
 </template>
 
 
---
+++ src/components/SpinnerLorad.vue
@@ -0,0 +1,45 @@
+
+<template>
+    <div class="spinner">
+      <div class="dot1"></div>
+      <div class="dot2"></div>
+    </div>
+  </template>
+  
+  <style>
+  .spinner {
+    width: 200px;
+    height: 200px;
+    position: relative;
+    margin: 100px auto;
+  }
+  
+  .spinner div {
+    width: 60%;
+    height: 60%;
+    background-color: #333;
+    border-radius: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    animation: bounce 2.0s infinite ease-in-out;
+  }
+  
+  .spinner .dot2 {
+    animation-delay: -1.0s;
+  }
+  
+  @keyframes bounce {
+    0%, 100% {
+      transform: scale(0.0);
+    }
+  
+    50% {
+      transform: scale(1.0);
+    }
+  }
+  </style>
+  
